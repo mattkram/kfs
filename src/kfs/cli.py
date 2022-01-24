2805a1c2d13a63a2018f70ff59753da48d276308
Matt Kramer <mkramer@anaconda.com>
2022-01-24 16:21:56 -0600

    feat: Add CLI command to initialize database

--- src/kfs/cli.py
+++ src/kfs/cli.py
@@ -1,10 +1,15 @@
+from pathlib import Path
+
 import typer
 from rich.console import Console
 
 from . import __version__
+from . import db
 
 console = Console()
 app = typer.Typer()
+db_app = typer.Typer()
+app.add_typer(db_app, name="db")
 
 
 @app.command()
@@ -13,6 +18,13 @@ def version() -> None:
     console.print(f"kfs version: {__version__}", style="bold green")
 
 
-@app.command()
-def dummy() -> None:  # pragma: no cover
-    """Temporarily here to make version a sub-command."""
+@db_app.command()
+def init(
+    path: Path = typer.Argument(lambda: Path.cwd() / "kfs.sqlite3"),
+) -> None:
+    """Initialize a new database."""
+    path = path.resolve()
+    path.parent.mkdir(exist_ok=True, parents=True)
+
+    console.print(f"Initializing the database at {path}")
+    db.init(url=f"sqlite:///{path}")
