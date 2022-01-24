8888ae0aed11560b5b3a19c5623623692eb1c644
Matt Kramer <mkramer@anaconda.com>
2022-01-24 15:06:20 -0600

    feat: Add database models for files and tags

--- src/kfs/db.py
+++ src/kfs/db.py
@@ -1,14 +1,53 @@
+import uuid
+from typing import List
 from typing import Optional
 
 from sqlalchemy.engine import Engine
+from sqlmodel import Field
+from sqlmodel import Relationship
+from sqlmodel import Session
 from sqlmodel import SQLModel
 from sqlmodel import create_engine
 
 engine: Optional[Engine] = None
 
 
+class FileTagAssociation(SQLModel, table=True):
+    file_id: Optional[uuid.UUID] = Field(
+        default=None, foreign_key="file.id", primary_key=True
+    )
+    tag_id: Optional[uuid.UUID] = Field(
+        default=None, foreign_key="tag.id", primary_key=True
+    )
+
+
+class File(SQLModel, table=True):
+    id: Optional[uuid.UUID] = Field(default_factory=uuid.uuid4, primary_key=True)
+    name: str
+    path: str
+
+    tags: List["Tag"] = Relationship(
+        back_populates="files", link_model=FileTagAssociation
+    )
+
+
+class Tag(SQLModel, table=True):
+    id: Optional[uuid.UUID] = Field(default_factory=uuid.uuid4, primary_key=True)
+    category: Optional[str]
+    value: str
+
+    files: List[File] = Relationship(
+        back_populates="tags", link_model=FileTagAssociation
+    )
+
+
 def init(url: str) -> None:
     global engine
-    engine = create_engine(url, echo=True)
+    engine = create_engine(url)
 
     SQLModel.metadata.create_all(engine)
+
+
+def get_session() -> Session:
+    """Create a new database session to use as a context manager."""
+    return Session(engine)
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import pytest
+from sqlmodel import select
 
 from kfs import db
 
@@ -20,7 +21,34 @@ def sqlite_url(base_dir: Path, sqlite_filename: str) -> str:
     return f"sqlite:///{base_dir / sqlite_filename}"
 
 
+@pytest.fixture(autouse=True)
+def database(sqlite_url: str) -> None:
+    db.init(sqlite_url)
+
+
 def test_init(base_dir: Path, sqlite_filename: str, sqlite_url: str) -> None:
     """After init, the database has been created and the file exists"""
-    db.init(sqlite_url)
     assert (base_dir / sqlite_filename).exists()
+
+
+def test_database() -> None:
+    with db.get_session() as session:
+        # Create a new file with a tag
+        tag = db.Tag(category="vendor", value="chevron")
+        file = db.File(name="test_file.csv", path="/some/directory", tags=[tag])
+        session.add(file)
+        session.commit()
+
+    with db.get_session() as session:
+        # Retrieve the file from database
+        read_file = session.exec(select(db.File)).one()
+
+        assert file is not read_file
+        assert read_file.name == "test_file.csv"
+        assert read_file.path == "/some/directory"
+        assert len(read_file.tags) == 1
+
+        read_tag = read_file.tags[0]
+        assert read_tag.category == "vendor"
+        assert read_tag.value == "chevron"
+        assert len(read_tag.files) == 1
