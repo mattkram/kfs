6aac7b6c2b8ff1eca35bad2550127c5b14f85c83
Matt Kramer <matthew.robert.kramer@gmail.com>
2024-11-23 15:47:30 -0600

    Update src/kfs/db.py

--- src/kfs/db.py
+++ src/kfs/db.py
@@ -158,7 +158,7 @@ def get_files_with_tag(tag: str) -> list[File]:
 
 
 @overload
-def get_tag(tag: str, create: Literal[False] = False) -> Tag | None: ...
+def get_tag(tag: str, create: Literal[False] = False) -> Tag | None: pass
 
 
 @overload
